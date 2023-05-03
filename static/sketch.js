17ddacfcc8f88dacce9a34bf133ec4ece6befaf1
lenardkone <lenardkone1@gmail.com>
2023-05-03 17:09:05 +0100

    sound added with p5

--- static/sketch.js
+++ static/sketch.js
@@ -3,6 +3,10 @@ const num = 20000;
 let img;
 const noiseScale = 0.004;
 
+//sound -- variables
+let sound, amplitude, button;
+//sound -- variables
+
 function setup() {
   var canvas = createCanvas(windowWidth, windowHeight);
   canvas.parent("canvas-wrapper");
@@ -10,10 +14,35 @@ function setup() {
   for (let i = 0; i < num; i++) {
     particles.push(createVector(random(width), random(height)));
   }
+
+  //sound -- create button & create amplitude object
+  sound = loadSound("assets/splash.wav");
+  button = createButton("play");
+  button.mouseClicked(togglePlay);
+  amplitude = new p5.Amplitude();
+  //sound -- create button & create amplitude object
 }
+
+// sound -- toggle function for button callback
+function togglePlay() {
+  if (sound.isPlaying()) {
+    sound.pause();
+  } else {
+    sound.loop();
+    amplitude = new p5.Amplitude();
+    amplitude.setInput(sound);
+  }
+}
+// sound -- toggle function for button callback
 
 function draw() {
   background(0, 30);
+
+  //sound -- getLevel variable
+  let level = amplitude.getLevel();
+  let waveFactor = map(level, 0, 1, 0, 400);
+  console.log(waveFactor);
+  //sound -- getLevel variable
 
   for (let i = 0; i < num; i++) {
     let p = particles[i];
@@ -43,8 +72,10 @@ function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
-var audio = new Audio("static/assets/splash.wav");
-audio.loop = true;
+//-----------NATIVE JAVASCRIPT CODE----------//
 
-audio.play();
-console.log(audio);
+// var audio = new Audio("static/assets/splash.wav");
+// audio.loop = true;
+
+// audio.play();
+// console.log(audio);
