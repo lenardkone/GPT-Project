e913ecef676ff0ffb87d3df3c401db36f0c4beb4
lenardkone <lenardkone1@gmail.com>
2023-05-08 15:36:53 +0100

    noise and audio adjustments

--- static/sketch.js
+++ static/sketch.js
@@ -1,69 +1,91 @@
+
+//NOISE -- variables
 let particles = [];
-const num = 20000;
-let img;
+const num = 12000;
+
+//higher value forms more dense waves
 const noiseScale = 0.004;
 
-//sound -- variables
-let sound, amplitude, button;
-//sound -- variables
+//SOUND -- variables
+let sound;
+let speechOutput;
+let button;
+let amplitude;
+let soundFactor;
+let micFactor;
+let volume;
+let micInput;
+
+function preload() {
+  sound = loadSound("static/assets/eyes-closed.wav");
+  speechOutput = createAudio("static/assets/speech - amy.mp3")
+}
 
 function setup() {
-  var canvas = createCanvas(windowWidth, windowHeight);
+  var canvas = createCanvas(windowWidth, windowHeight,);
   canvas.parent("canvas-wrapper");
 
   for (let i = 0; i < num; i++) {
     particles.push(createVector(random(width), random(height)));
   }
 
-  //sound -- create button & create amplitude object
-  sound = loadSound("assets/splash.wav");
-  button = createButton("play");
-  button.mouseClicked(togglePlay);
+  //SOUND -- create amplitude object & mic input
   amplitude = new p5.Amplitude();
-  //sound -- create button & create amplitude object
+  // micInput = new p5.AudioIn();
+  // micInput.start();
+
 }
 
-// sound -- toggle function for button callback
-function togglePlay() {
-  if (sound.isPlaying()) {
-    sound.pause();
-  } else {
-    sound.loop();
-    amplitude = new p5.Amplitude();
-    amplitude.setInput(sound);
-  }
-}
-// sound -- toggle function for button callback
 
 function draw() {
   background(0, 30);
 
-  //sound -- getLevel variable
-  let level = amplitude.getLevel();
-  let waveFactor = map(level, 0, 1, 0, 400);
-  console.log(waveFactor);
-  //sound -- getLevel variable
+//SOUND -- get the volume level of the audio and map it to a variable  
 
+  // let micLevel = micInput.getLevel();
+  let soundLevel = amplitude.getLevel();
+  soundFactor = map(soundLevel, 0, 0.4, 1, 5);
+  // micFactor = map((micLevel * 100), 0, 1.2, 0.2, 1);
+
+  // console.log("sound: " + soundFactor);
+  // console.log("mic " + micFactor);
+
+
+//NOISE -- add particles and flow field
   for (let i = 0; i < num; i++) {
     let p = particles[i];
-    stroke(204, 198, 242);
+    stroke(198, 192, 244);
     strokeWeight(1);
     point(p.x, p.y);
-    let n = noise(p.x * noiseScale, p.y * noiseScale);
+    let noiseVar = noise(p.x * noiseScale, p.y * noiseScale);
 
-    let a = TAU * n;
-    p.x += -cos(a);
-    p.y += sin(a);
+    let a = TAU * noiseVar;
+    p.x += cos(a) ;
+    p.y += -sin(a) ;
+  
+  //if the particles go off screen assign random position on canvas
     if (!onScreen(p)) {
-      p.x = random(width);
-      p.y = random(height);
+      p.x = random( width);
+      p.y = random( height);
     }
   }
-}
+
+  //if the audio Sound is higher than 2 the flow field changes directions
+  if(soundFactor >= 3 && soundFactor <= 3.01){
+    noiseSeed(random(1000));
+    }
+    
+
+  }
+
+
+//NOISE mouse action for background
 function mouseReleased() {
   noiseSeed(random(1000));
 }
 
+
+//requirements for particles being on screen
 function onScreen(v) {
   return v.x >= 0 && v.x <= width && v.y >= 0 && v.y <= height;
 }
@@ -72,10 +94,65 @@ function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
-//-----------NATIVE JAVASCRIPT CODE----------//
 
-// var audio = new Audio("static/assets/splash.wav");
-// audio.loop = true;
+document.getElementById('promptField').addEventListener('click', startAudio);
 
-// audio.play();
-// console.log(audio);
+function startAudio() {
+  if(sound.isLoaded()){
+  sound.play();
+  speeechOutput = speechOutput.speed(0.9);
+  // speeechOutput = speechOutput.amp(0.5);
+  speechOutput.play();
+  document.getElementsByClassName("audioBtn")[0].innerHTML = "<i class='fa-solid fa-volume-high fa-xs' style='color: #ffffff;'></i>";
+
+  }
+}
+
+
+document.getElementsByClassName('audioBtn')[0].addEventListener('click', togglePlay);
+// SOUND -- toggle function for button
+function togglePlay() {
+
+  //if sound is playing and the button is pressed change icon and stop the audio
+  if (sound.isPlaying()) {
+    document.getElementsByClassName("audioBtn")[0].innerHTML = "<i class='fa-solid fa-volume-xmark fa-xs' style='color: #fff;'></i>";
+    sound.stop();
+
+    //if it's pressed again start playing the sound in a loop and change button icon
+  } else if(!sound.isPlaying()) {
+    sound.play();
+
+    amplitude = new p5.Amplitude();
+    amplitude.setInput(sound);
+    document.getElementsByClassName("audioBtn")[0].innerHTML = "<i class='fa-solid fa-volume-high fa-xs' style='color: #ffffff;'></i>";
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// DEPRICATED
+
+// volume = sound.getAmp();
+// let v = Math.round((volume + Number.EPSILON) * 100) / 100
+
+// for(v; v === 0; v-0.001){
+//   if(v<=0.01){
+//     sound.stop();
+//   }
+// }
